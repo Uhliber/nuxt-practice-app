6900668527a06b82cd6319120e2252007fc432a2
Uhliber <olvrcp@gmail.com>
2023-10-01 17:32:07 +0800

    add pages and layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav>
+        <NuxtLink to="/">
+            Home
+        </NuxtLink>
+    </nav>
+    <main>
+        <slot />
+    </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
